f49804319ec7608fff405140f4ea605ff331a54a
Dmitrii <dmitriy.falko@gmail.com>
2023-02-12 16:17:39 +0100

    Prepare for big tests

--- src/containers/neo4j/command-existing-disk.sh
+++ src/containers/neo4j/command-existing-disk.sh
@@ -4,7 +4,7 @@ yc compute instance create-with-container \
   --name neo4j-vm \
   --zone ru-central1-a \
   --create-boot-disk size=30 \
-  --attach-disk disk-name=data-disk-small,device-name=coi-data \
+  --attach-disk disk-name=data-disk-big,device-name=coi-data \
   --attach-disk disk-name=import-disk,device-name=coi-import \
   --network-interface subnet-name=testing,nat-ip-version=ipv4 \
   --ssh-key ~/.ssh/id_ed25519.pub \
--- src/containers/neo4j/import-command.sh
+++ src/containers/neo4j/import-command.sh
@@ -1,10 +1,10 @@
-docker exec --interactive --tty e90f293a6482 neo4j-admin database import full \
+docker exec --interactive --tty 598a2d9c972c neo4j-admin database import full \
   --overwrite-destination \
-  --nodes=OrganizationUnit=import/small-set-with-headers/organization_unit_header.csv,import/small-set-with-headers/organization-units.csv \
-  --relationships=CHILD_OF=import/small-set-with-headers/ou_to_ou_header.csv,import/small-set-with-headers/ou_to_ou.csv \
-  --nodes=Outlet=import/small-set-with-headers/outlets_header.csv,import/small-set-with-headers/outlets.csv \
-  --relationships=BELONG_TO=import/small-set-with-headers/outlets_to_ou_header.csv,import/small-set-with-headers/outlets_to_ou.csv \
-  --nodes=Device=import/small-set-with-headers/devices_header.csv,import/small-set-with-headers/devices.csv \
-  --relationships=LOCATED_AT=import/small-set-with-headers/device_to_outlet_header.csv,import/small-set-with-headers/devices_to_outlets.csv \
-  --nodes=Worker=import/small-set-with-headers/workers_header.csv,import/small-set-with-headers/workers.csv \
-  --relationships=WORK_IN=import/small-set-with-headers/worker_to_ou_header.csv,import/small-set-with-headers/workers_to_ou.csv
+  --nodes=OrganizationUnit=import/big-set-with-headers/organization_unit_header.csv,import/big-set-with-headers/organization-units.csv \
+  --relationships=CHILD_OF=import/big-set-with-headers/ou_to_ou_header.csv,import/big-set-with-headers/ou_to_ou.csv \
+  --nodes=Outlet=import/big-set-with-headers/outlets_header.csv,import/big-set-with-headers/outlets.csv \
+  --relationships=BELONG_TO=import/big-set-with-headers/outlets_to_ou_header.csv,import/big-set-with-headers/outlets_to_ou.csv \
+  --nodes=Device=import/big-set-with-headers/devices_header.csv,import/big-set-with-headers/devices.csv \
+  --relationships=LOCATED_AT=import/big-set-with-headers/device_to_outlet_header.csv,import/big-set-with-headers/devices_to_outlets.csv \
+  --nodes=Worker=import/big-set-with-headers/workers_header.csv,import/big-set-with-headers/workers.csv \
+  --relationships=WORK_IN=import/big-set-with-headers/worker_to_ou_header.csv,import/big-set-with-headers/workers_to_ou.csv
