d2f6d81c4a7eb4ba668dfc578e5f918b3fca1a3a
Dmitrii <dmitriy.falko@gmail.com>
2023-03-01 21:15:00 +0100

    Migrations for Clickhouse update

--- src/migrations/inspections.sql
+++ src/migrations/inspections.sql
@@ -1,3 +1,32 @@
+
+
+CREATE TABLE inspections.kafka_stream
+(
+    `inspection_id` UUID,
+    `worker_id` UUID,
+    `device_id` UUID,
+    `event_type` LowCardinality(String),
+    `event_data` String,
+    `datetime` DateTime64
+)
+ENGINE=Kafka
+SETTINGS
+    kafka_broker_list = 'rc1a-g31oe8fl2r0n9vmc.mdb.yandexcloud.net:9091',
+    kafka_topic_list = 'inspections_events',
+    kafka_group_name = 'sample_group',
+    kafka_format = 'JSONEachRow';
+
+ CREATE MATERIALIZED VIEW inspections.materialize_kafka_events TO inspections.event_log
+ AS SELECT
+ 	inspection_id,
+ 	worker_id,
+ 	device_id,
+ 	event_type,
+ 	event_data as `payload`,
+ 	`datetime`
+FROM inspections.kafka_stream
+
+
 CREATE TABLE inspections.event_log
 (
     `inspection_id` UUID,
@@ -20,19 +49,25 @@ CREATE TABLE inspections.materialized_start_events
     `start_time` DateTime64
 )
 ENGINE=MergeTree
-PRIMARY KEY (`inspection_id`, `start_time`);
+PRIMARY KEY (`inspection_id`, `start_time`)
+;
+
+PARTITION BY toYYYYMMDD(`start_time`)
+TTL toYYYYMMDD(`start_time`) + INTERVAL 1 DAY DELETE;
 
 
 CREATE MATERIALIZED VIEW inspections.materialize_start_event TO inspections.materialized_start_events
 AS SELECT
 	inspection_id,
 	worker_id,
-	JSONExtract(JSONExtract(payload, 'worker', 'String'), 'materialized_path', 'Array(UUID)') as worker_path,
+	JSONExtract(JSONExtract(replaceAll(payload, '\'', '"'), 'worker', 'String'), 'materialized_path', 'Array(UUID)') as worker_path,
 	device_id,
-	JSONExtract(JSONExtract(payload, 'device', 'String'), 'materialized_path', 'Array(UUID)') as device_path,
+	JSONExtract(JSONExtract(replaceAll(payload, '\'', '"'), 'device', 'String'), 'materialized_path', 'Array(UUID)') as device_path,
 	`datetime` as start_time
 FROM inspections.event_log WHERE event_type = 'START';
 
+
+/* TODO: add TTL */
 CREATE TABLE inspections.materialized_end_events
 (
 	`inspection_id` UUID,
@@ -47,6 +82,7 @@ AS SELECT
 	`datetime` as end_time
 FROM inspections.event_log WHERE event_type = 'END';
 
+
 CREATE TABLE inspections.materialized_inspections
 (
 	`inspection_id` UUID,
@@ -59,7 +95,7 @@ CREATE TABLE inspections.materialized_inspections
     `data` Array(String)
 )
 ENGINE=MergeTree
-PRIMARY KEY (`inspection_id`, `end_time`);
+PRIMARY KEY (`inspection_id`, `end_time`); /* Do we need worker ID or something else here? */
 
 
 CREATE MATERIALIZED VIEW inspections.materialize_inspection TO inspections.materialized_inspections
@@ -91,6 +127,7 @@ INNER JOIN (
 ) as d ON d.inspection_id = ee.inspection_id
 ;
 
+
 CREATE TABLE inspections.materialized_result_events
 (
 	`inspection_id` UUID,
@@ -105,10 +142,11 @@ CREATE MATERIALIZED VIEW inspections.materialize_result TO inspections.materiali
 AS SELECT
 	inspection_id,
 	`datetime` as `result_time`,
-	JSONExtract(payload, 'result', 'String') as `result`,
-	JSONExtract(payload, 'data', 'String') as `result_data`
+	JSONExtract(replaceAll(payload, '\'', '"'), 'result', 'String') as `result`,
+	JSONExtract(replaceAll(payload, '\'', '"'), 'data', 'String') as `result_data`
 FROM inspections.event_log
 WHERE event_type = 'RESULT';
+
 
 
 CREATE TABLE inspections.materialized_inspections_with_results
@@ -149,3 +187,7 @@ INNER JOIN (
 	WHERE mi.inspection_id IN (SELECT mre.inspection_id  FROM materialized_result_events mre)
 ) as ins
 ON ins.inspection_id = mre.inspection_id;
+
+
+SET stream_like_engine_allow_direct_select = 1;
+select * from inspections.kafka_stream ks;
